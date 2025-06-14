4ef97524eeb7821454b83efa257ae57afd737d54
西瓜呱呱叫 <windows_2386104975@qq.com>
2025-06-14 12:09:06 +0800

    添加项目文件。

---
+++ lockfree_queue.cpp
@@ -0,0 +1,12 @@
+﻿// lockfree_queue.cpp: 定义应用程序的入口点。
+//
+
+#include "lockfree_queue.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
---
+++ lockfree_queue.h
@@ -0,0 +1,27 @@
+﻿// lockfree_queue.h: 标准系统包含文件的包含文件
+// 或项目特定的包含文件。
+
+#pragma once
+
+#include <iostream>
+#include <atomic>
+namespace lockfree {
+	template<typename T>
+	struct node {
+		T data;
+		std::atomic<node*>next;
+		node(const T & value) :data(value),next(nullptr) {}
+	};
+
+
+	template<typename T>
+	class lockfree_queue {
+	public:
+		lockfree_queue() {
+
+		}
+	private:
+		std::atomic<node<T>*> head_;
+		std::atomic<node<T>*> tail_;
+	};
+}
